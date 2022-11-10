6da12b5ae38985620cfbfb5a38e0587b99f36e85
AmadulHaque <mdamadulhaque8@gmail.com>
2022-11-10 13:01:58 +0600

    ongoscustomvalidation commit

--- src/models/StudentModel.js
+++ src/models/StudentModel.js
@@ -3,7 +3,21 @@ const mongoose = require('mongoose');
 const DataSchema = mongoose.Schema({
     Name:String,
     Roll:String,
-    Class:String
+    Class:String,
+    Phone:{
+        type:String,
+        validate:{
+            validator:function(value) {
+                if(value.length===11){
+                    return true;
+                }else {
+                    return false;
+                }
+            },
+            message:"Number required 11 digits",
+        },
+
+    }
 });
 
 const StudentsModel = mongoose.model('students',DataSchema);
